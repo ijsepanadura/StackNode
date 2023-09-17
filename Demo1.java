d5516d14880782a7cbf4c81b7f85a599bdb79258
Ishan Lahiru <ishanlahiru2012@gmail.com>
2023-09-17 21:37:08 +0530

    NodeStack

---
+++ Demo1.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Stack10 stack = new Stack10();
+        System.out.println(stack.peek());
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.print();
+        System.out.println(stack.pop());
+        stack.print();
+        
+    }
+}
+class Stack10{
+    Node head;
+    Node tail;
+
+
+    void push(int number){
+        if(head==null){
+            head= new Node(number);
+            tail = head;         
+        }else{   
+            tail = new Node(number, tail);
+        }
+
+    }
+    int peek(){
+        return head == null ? -1: head.value;
+    }
+    int pop(){
+        if(head == null){
+            return -1;
+        }else{
+            int pass= tail.value;
+            tail = tail.next;
+            return pass;
+            
+        }
+    }
+
+    void print(){
+        if(tail==null){
+            System.out.println("[]");
+        }else{
+            // System.out.print("[");
+            // Node temp = tail;
+            // while(temp != null){
+            //     System.out.print(temp.value+",");
+            //     temp = temp.next;
+            // }
+            // System.out.println("\b]");
+            ArrayList<Integer> listValue = new ArrayList<>();
+            Node temp = tail;
+            while(temp != null){
+                listValue.add(temp.value);
+                temp = temp.next;
+            }
+            Collections.reverse(listValue);
+            System.out.println(listValue);
+
+        }
+
+    }
+
+}
+
+class Node{
+    int value;
+    Node next;
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+    public Node(int number){
+        this.value = number;
+    }
+
+    
+}
